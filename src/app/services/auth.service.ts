b5da2380c36a8b442f95813d8e11352c0e51c3f6
Enrique Teruel <enriqueteruel@MacBook-Air-de-Enrique.local>
2022-05-17 23:09:39 +0200

    feat(auth):Create database document for each user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireAuth} from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+import { map } from 'rxjs';
+import { User } from '../core/models/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +13,15 @@ export class AuthService {
     this.auth.authState.subscribe((fuser) => console.log(fuser));
   }
 
-  constructor(private auth: AngularFireAuth) {}
+  constructor(private auth: AngularFireAuth, private fs: AngularFirestore) {}
 
   newUser(name: string, email: string, password: string) {
-    return this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        const newUser = new User(user!.uid, name, email);
+        return this.fs.doc(`${user!.uid}/user`).set({...newUser})
+      });
   }
 
   login(email: string, password: string) {
@@ -21,5 +30,9 @@ export class AuthService {
 
   logout() {
     return this.auth.signOut();
+  }
+
+  IsAuth() {
+    return this.auth.authState.pipe(map((fuser) => fuser != null));
   }
 }
